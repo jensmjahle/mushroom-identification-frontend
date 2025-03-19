170fbed27e53edc9fa847a227204f2d10ceeda72
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-03-19 13:52:17 +0100

    add sendNewUserRequest method in apiService.js

---
+++ src/services/apiService.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8080';
+
+export const sendNewUserRequest = async (text,  images) => {
+  const requestBody = {
+    text: text,
+    images: images
+  };
+  
+  try {
+    const response = await axios.post(`${API_URL}/api/requests/create`,
+        requestBody);
+    return response.data;
+  } catch (error) {
+      console.error('Error while sending new user request', error);
+      throw error;
+    }
+  }
+  
+  
