fabc773b5172d5d5b9a812c249d6e255a6ccf9c2
anderebe <anderebe@stud.ntnu.no>
2025-03-19 11:17:36 +0100

    add a simple homepage with a form to input a string

---
+++ src/components/HomePage.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="home-page">
+      <h1>Welcome to the Mushroom Identifier</h1>
+      
+      <input
+        v-model="userInput"
+        type="text"
+        placeholder="Enter something..."
+      />
+      
+      <p>You typed: {{ userInput }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage',
+    data() {
+      return {
+        userInput: ''
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .home-page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+  }
+  
+  input {
+    padding: 10px;
+    margin-top: 20px;
+    width: 300px;
+    font-size: 16px;
+  }
+  
+  p {
+    margin-top: 20px;
+    color: #333;
+  }
+  </style>
+  
