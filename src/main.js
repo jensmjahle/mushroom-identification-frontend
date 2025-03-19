df4d4e2b8120384877b0e472d087d7a09b22acc4
anderebe <anderebe@stud.ntnu.no>
2025-03-19 13:27:21 +0100

    Add the tailwindcss

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './assets/style.css'
 import App from './App.vue'
+import './assets/tailwind.css'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
