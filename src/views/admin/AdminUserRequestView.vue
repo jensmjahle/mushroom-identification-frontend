0f7e3ea3bd8b79c327e1e318153866b112ebc27b
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-03-25 16:06:28 +0100

    add RequestStatusBox.vue component to AdminUserRequestView.vue

--- src/views/admin/AdminUserRequestView.vue
+++ src/views/admin/AdminUserRequestView.vue
@@ -1,12 +1,27 @@
 <template>
-  <ChatBox :userRequestId="userRequestId" />
+  <div>
+    <RequestStatusBox v-if="userRequest" :request="userRequest" />
+    <ChatBox v-if="userRequestId" :userRequestId="userRequestId" />
+  </div>
 </template>
 
 <script setup>
 import ChatBox from '../../components/ChatBox.vue';
 
 import { useRoute } from 'vue-router';
+import RequestStatusBox from "../../components/RequestStatusBox.vue";
+import {onMounted, ref} from "vue";
+import {getUserRequestAdmin} from "../../services/apiService.js";
 
 const route = useRoute();
 const userRequestId = route.params.userRequestId;
+const userRequest = ref(null);
+const token = sessionStorage.getItem('jwt');
+
+onMounted(() => {
+  console.log('User Request ID:', userRequestId);
+  getUserRequestAdmin(userRequestId, token).then((data) => {
+    userRequest.value = data;
+  });
+})
 </script>
