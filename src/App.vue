39854815b162e0b7e3712c91f9e879f4e6b2cd33
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-03-25 12:37:51 +0100

    add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <script setup>
 import NavBar from "./components/NavBar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <NavBar />
-  <div id="main">
-    <router-view />
-  </div>
-  
+  <router-view />
+  <Footer />
 </template>
 
-<style scoped>
-
-</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="absolute left-0 right-0 bg-bg text-textAlt py-4 mt-8">
+  <div class="text-center px-4">
+      <p>&copy; 2025 Fleinsoppkontroll.no</p>
+    </div>
+  </footer>
+</template>
