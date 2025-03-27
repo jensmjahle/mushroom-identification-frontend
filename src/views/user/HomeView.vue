2c929c5b618921f174b00e78c0afde42c646f68b
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-03-27 12:41:53 +0100

    change logo and app icon

--- src/views/user/HomeView.vue
+++ src/views/user/HomeView.vue
@@ -1,3 +1,16 @@
+
 <template>
-  <h1>Home</h1>
+  <div class="flex flex-col items-center justify-center min-h-screen bg-bg px-4 text-center">
+    <img
+        src="../../../public/assets/logo.svg"
+        alt="Fleinsoppkontroll Logo"
+        class="w-56 h-auto mb-6"
+    />
+    <h1 class="text-2xl font-semibold text-textAlt">This site is currently under construction. Thanks for your patience!</h1>
+    
+  </div>
 </template>
+<script setup>
+</script>
+
+
