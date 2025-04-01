e8717bbaa36eefa2c1798328a14cd17a1f4befca
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-04-01 12:24:45 +0200

    add change theme dynamically

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue';
 import './assets/tailwind.css';
 import i18n, { updateI18nLocale } from './locales/i18n'; // Import i18n and update function
 import router from './router';
+import { initTheme } from './composables/useTheme'
 
 // Create the Pinia store instance
 const pinia = createPinia();
@@ -16,6 +17,8 @@ const app = createApp(App);
 app.use(pinia);
 app.use(i18n);
 app.use(router);
+
+initTheme()
 
 // Ensure the language updates dynamically when the app starts
 updateI18nLocale(); 
