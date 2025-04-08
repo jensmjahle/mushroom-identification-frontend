395b61142b102cfeac4e828b3fc05af3f0072670
anderebe <anderebe@stud.ntnu.no>
2025-04-08 13:03:49 +0200

    Fix HomeView to not use up to much width and widening the display-card

--- src/views/user/HomeView.vue
+++ src/views/user/HomeView.vue
@@ -3,20 +3,20 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center h-full w-full bg-gray-100 p-6 text-center">
+  <div class="flex flex-col items-center justify-center h-full w-full bg-bg1 p-6 text-center">
     <img src="/assets/logo.svg" class="w-48 mb-6" alt="Fleinsoppkontroll logo" />
 
     <p class="text-black text-md font-semibold mb-6 max-w-lg w-[60%]">
       Usikker på om du har plukket fleinsopp? Send inn en gratis anonym forespørsel her:
     </p>
 
-    <RouterLink class="user-button bg-[#F28F3B] hover:bg-[#2F3C2C] hover:text-white" :to="{ name: 'new-request' }">
+    <RouterLink class="user-button bg-button3 hover:bg-button3-hover hover:text-white" :to="{ name: 'new-request' }">
         Send forespørsel
     </RouterLink>
 
 
     <p class="text-black text-sm mt-6">
-      Eller <RouterLink to="user/support" class="underline text-[#F28F3B] hover:text-[#2F3C2C]">få hjelp fra vår support</RouterLink>
+      Eller <RouterLink to="user/support" class="underline text-button3] hover:text-button3-hover">få hjelp fra vår support</RouterLink>
     </p>
   </div>
 </template>
