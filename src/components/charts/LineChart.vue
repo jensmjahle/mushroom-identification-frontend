603b5de12af1bb42451259c5f61e670103bfe204
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-04-08 12:26:02 +0200

    fix linechart not displaying the line

--- src/components/charts/LineChart.vue
+++ src/components/charts/LineChart.vue
@@ -1,12 +1,30 @@
 <script setup>
 import { Line } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, PointElement, CategoryScale, LinearScale } from 'chart.js'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+import { computed } from 'vue'
 
-ChartJS.register(Title, Tooltip, Legend, LineElement, PointElement, CategoryScale, LinearScale)
+ChartJS.register(
+    Title,
+    Tooltip,
+    Legend,
+    LineElement,
+    PointElement,
+    CategoryScale,
+    LinearScale
+)
 
 const props = defineProps({ data: Array })
 
-const chartConfig = {
+const chartConfig = computed(() => ({
   labels: props.data.map(p => p.date),
   datasets: [{
     label: 'Completed Requests',
@@ -15,7 +33,7 @@ const chartConfig = {
     borderColor: '#4ade80',
     tension: 0.3
   }]
-}
+}))
 </script>
 
 <template>
