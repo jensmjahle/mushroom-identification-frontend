736f1301aa8e06a9a840f7d37054ad71edc7f9ec
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-04-08 12:12:52 +0200

    add LineChart component

---
+++ src/components/charts/LineChart.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Line } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, PointElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, LineElement, PointElement, CategoryScale, LinearScale)
+
+const props = defineProps({ data: Array })
+
+const chartConfig = {
+  labels: props.data.map(p => p.date),
+  datasets: [{
+    label: 'Completed Requests',
+    data: props.data.map(p => p.count),
+    fill: false,
+    borderColor: '#4ade80',
+    tension: 0.3
+  }]
+}
+</script>
+
+<template>
+  <Line :data="chartConfig" />
+</template>
