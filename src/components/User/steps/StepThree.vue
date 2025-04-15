bba26890fa88990f85658d0c767ec24fb0034ece
anderebe <anderebe@stud.ntnu.no>
2025-04-15 14:52:23 +0200

    Remove the back button on the last step of send in

--- src/components/User/steps/StepThree.vue
+++ src/components/User/steps/StepThree.vue
@@ -3,7 +3,6 @@
       <h2 class="text-2xl font-semibold">Takk!</h2>
       <p class="text-center max-w-md">Du vil få svar så snart en ekspert har vurdert bildet ditt.</p>
       <div class="flex gap-4">
-        <button @click="$emit('back')" class="user-button bg-gray-400 hover:bg-gray-500">Tilbake</button>
         <router-link to="/user" class="user-button">Til forsiden</router-link>
       </div>
     </div>
