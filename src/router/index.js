923687807b6d20ef14f0eadb14180f538a477aad
anderebe <anderebe@stud.ntnu.no>
2025-04-17 11:30:30 +0200

    Create the supportview and the correct component in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import NewRequestView from "../views/user/NewRequestView.vue";
 import UserLoginView from "@/views/user/UserLoginView.vue";
 import StatisticsView from "@/views/admin/StatisticsView.vue";
 import BecomeMemberView from "@/views/user/BecomeMemberView.vue";
+import SupportView from '../views/user/SupportView.vue';
 
 const routes = [
   { 
@@ -51,7 +52,7 @@ const routes = [
       {
         path: 'support',
         name: 'support',
-        component:NewRequestView,
+        component:SupportView,
       },
       {
         path: 'login',
