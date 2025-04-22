7d96fad89cf7b2caef8f6d2bd11567e7e843b96c
anderebe <anderebe@stud.ntnu.no>
2025-04-22 10:54:19 +0200

    Add language compatability for the home page

--- src/views/user/HomeView.vue
+++ src/views/user/HomeView.vue
@@ -1,22 +1,26 @@
-<script setup lang="ts">
-import { RouterLink } from 'vue-router';
-</script>
-
 <template>
   <div class="flex flex-col items-center justify-center h-full w-full p-6 text-center">
     <img src="/assets/logo.svg" class="w-48 mb-6" alt="Fleinsoppkontroll logo" />
 
     <p class="text-text1 text-md font-semibold mb-6 max-w-lg w-[60%]">
-      Usikker på om du har plukket fleinsopp? Send inn en gratis anonym forespørsel her:
+      {{ t('home.description') }}
     </p>
 
     <RouterLink class="user-button" :to="{ name: 'new-request' }">
-        Send forespørsel
+      {{ t('home.sendRequest') }}
     </RouterLink>
 
-
     <p class="text-text1 text-sm mt-6">
-      Eller <RouterLink to="user/support" class="underline text-text1 hover:text-text1">få hjelp fra vår support</RouterLink>
+      {{ t('home.or') }} <RouterLink to="user/support" class="underline text-text1 hover:text-text1">
+        {{ t('home.getSupport') }}
+      </RouterLink>
     </p>
   </div>
 </template>
+
+  <script setup lang="ts">
+  import { useI18n } from 'vue-i18n';
+  import { RouterLink } from 'vue-router';
+  
+  const { t } = useI18n();
+  </script>
