421c4fb0d5c91fe4d8ad068730f2f68f835d3ef4
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-04-23 09:46:18 +0200

    add mobile hamburger menu component; integrate with AdminSideMenuContent in AppAdminLayout.vue

--- src/layouts/AppAdminLayout.vue
+++ src/layouts/AppAdminLayout.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex flex-col min-h-screen">
-    <MobileAdminHamburgerMenu />
+    <MobileHamburgerMenu>
+      <AdminSideMenuContent />
+    </MobileHamburgerMenu>
+    
     <div class="flex flex-1">
       <SideMenu />
       <main class="flex w-full pt-[72px]">
@@ -14,4 +17,6 @@
 import NavBar from '../components/navigation/NavBar.vue';
 import SideMenu from "../components/navigation/AdminSideMenu.vue";
 import MobileAdminHamburgerMenu from "@/components/navigation/MobileAdminHamburgerMenu.vue";
+import MobileHamburgerMenu from "@/components/navigation/MobileHamburgerMenu.vue";
+import AdminSideMenuContent from "@/components/navigation/AdminSideMenuContent.vue";
 </script>
