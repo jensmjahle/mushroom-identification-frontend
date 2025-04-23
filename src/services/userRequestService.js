604eadec722c0e45208c020318a773d21ff55681
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-04-23 14:14:48 +0200

    refactor sendNewUserRequest function; update parameter structure and add detailed JSDoc comments

--- src/services/userRequestService.js
+++ src/services/userRequestService.js
@@ -2,10 +2,30 @@ import axios from '@/config/axiosConfig'
 import { getAuthHeaders } from '@/utils/authHeaders'
 import {useToast} from "vue-toastification";
 
-export const sendNewUserRequest = async (text, images) => {
+/**
+ * Send a new user request. 
+ *
+ * @example
+ * const text = 'This mushroom was found near the woods.';
+ * const mushrooms = [
+ *   { images: [file1, file2] },
+ *   { images: [file3] }
+ * ];
+ *
+ * @param text the text of the request
+ * @param mushrooms the mushrooms to send, each with images
+ * @returns {Promise<*|null>} the response data or null if an error occurred
+ */
+export const sendNewUserRequest = async (text, mushrooms) => {
   const formData = new FormData()
+
   formData.append('text', text)
-  images.forEach(img => formData.append('images', img))
+  
+  mushrooms.forEach((mushroom, mIndex) => {
+    mushroom.images.forEach((img, iIndex) => {
+      formData.append(`mushrooms[${mIndex}].images`, img)
+    })
+  })
 
   try {
     const response = await axios.post('/api/requests/create', formData, {
