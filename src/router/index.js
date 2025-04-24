23947a9029c02c6b1d7ed3cac11b8a588790b2cd
anderebe <anderebe@stud.ntnu.no>
2025-04-24 11:28:32 +0200

    Make the root url redirect to the user page

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ const routes = [
     path: '/', 
     name: 'Construction', 
     component: UnderConstructionView,
-    redirect: '/user/home'
+    redirect: '/user'
   },
   {
     path: '/user',
