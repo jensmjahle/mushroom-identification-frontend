056767a123cee88c32abfe22815ba7ae5cb37e1f
anderebe <anderebe@stud.ntnu.no>
2025-04-25 12:06:19 +0200

    Fix not delivered reference code bug

--- src/views/user/NewRequestView.vue
+++ src/views/user/NewRequestView.vue
@@ -5,24 +5,24 @@
     </div>
     <div class="flex items-center justify-center w-full h-full">
       <StepOne v-if="currentStep === 1" @next="goToStepTwo" />
-      <StepTwo v-else-if="currentStep === 2" :referenceCode="referenceCode" @next="currentStep++" />
-      <StepThree v-else-if="currentStep === 3" :userCode="referenceCode" @back="currentStep--" />
+      <StepTwo v-else-if="currentStep === 2" :referenceCode="userCode" @next="currentStep++" @back="currentStep--" />
+      <StepThree v-else-if="currentStep === 3" :referenceCode="userCode" @back="currentStep--" />
     </div>
   </div>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
-  import StepIndicator from '@/components/User/steps/StepIndicator.vue'
-  import StepOne from '@/components/User/steps/StepOne.vue'
-  import StepTwo from '@/components/User/steps/StepTwo.vue'
-  import StepThree from '@/components/User/steps/StepThree.vue'
-  
-  const currentStep = ref(1)
-  const referenceCode = ref('')
-  
-  function goToStepTwo(payload) {
-    referenceCode.value = payload.referenceCode
-    currentStep.value++
-  }
+import { ref } from 'vue'
+import StepIndicator from '@/components/User/steps/StepIndicator.vue'
+import StepOne from '@/components/User/steps/StepOne.vue'
+import StepTwo from '@/components/User/steps/StepTwo.vue'
+import StepThree from '@/components/User/steps/StepThree.vue'
+
+const currentStep = ref(1)
+const userCode = ref(null)
+
+function goToStepTwo(result) {
+  userCode.value = result
+  currentStep.value = 2
+}
 </script>
