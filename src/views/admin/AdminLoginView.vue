b2b7adea20c1107a50506373dd4ecb3e30e3c1ed
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-04-25 15:18:13 +0200

    feat: redesign AdminLoginForm and AdminLoginView for improved user experience and accessibility

--- src/views/admin/AdminLoginView.vue
+++ src/views/admin/AdminLoginView.vue
@@ -1,36 +1,28 @@
-
 <template>
-  <h1>Fleinsoppkontroll.no</h1>
   <AdminLoginForm :onLogin="loginUser" />
 </template>
 
 <script setup>
 import { useRouter } from "vue-router";
-import AdminLoginForm from "../../components/AdminLoginForm.vue";
-import {loginAdmin} from "@/services/authService";
+import AdminLoginForm from "@/components/AdminLoginForm.vue";
+import { loginAdmin } from "@/services/authService";
 
 const router = useRouter();
-
 
 const loginUser = async (username, password) => {
   try {
     const response = await loginAdmin(username, password);
-    console.log(response);
-
     const token = response.data.token;
 
     if (token) {
-      alert("Login successful!");
       sessionStorage.setItem("jwt", token);
-      await router.push("/admin/dashboard");
+      await router.push({ name: "admin-dashboard" }); // use route name
     } else {
       throw new Error("Invalid credentials");
     }
   } catch (error) {
     console.error("Login failed:", error);
-    alert("Login failed. Please check your credentials.");
+    throw error;
   }
 };
-
 </script>
-
