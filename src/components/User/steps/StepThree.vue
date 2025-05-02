ad47fb1667672266b1db8671c7138ddeab05d510
anderebe <anderebe@stud.ntnu.no>
2025-05-02 10:55:05 +0200

    Fix the homepage button not working in stepThree

--- src/components/User/steps/StepThree.vue
+++ src/components/User/steps/StepThree.vue
@@ -3,13 +3,11 @@
     <h2 class="text-2xl font-semibold">{{ t('submit.thankYou') }}</h2>
     <p class="text-center max-w-md">{{ t('submit.thankYouHint') }}</p>
     <div class="flex gap-4">
-      <BaseButton
-        as="router-link"
-        to="/user"
-        class="px-5 py-3 text-base"
-      >
-        {{ t('submit.toFront') }}
-      </BaseButton>
+      <RouterLink :to="{ name: 'home' }">
+        <BaseButton variant="1">
+          {{ t('submit.toFront') }}
+        </BaseButton>
+      </RouterLink>
     </div>
   </div>
 </template>
