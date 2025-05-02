07945829fd6ced3fa2f9e25ba1784d2523b2370f
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-05-02 12:20:01 +0200

    feat: add mushrooms column to StatisticsView and adjust column widths

--- src/views/admin/StatisticsView.vue
+++ src/views/admin/StatisticsView.vue
@@ -39,9 +39,10 @@ import { onMounted, ref, watch } from "vue";
 import { useToast } from "vue-toastification";
 
 const columns = [
-  { label: 'Request ID', key: 'userRequestId', class: 'col-span-6' },
+  { label: 'Request ID', key: 'userRequestId', class: 'col-span-5' },
   { label: 'Last Updated', key: 'updatedAt', class: 'col-span-3' },
-  { label: 'Status', key: 'status', class: 'col-span-3' }
+  { label: 'Status', key: 'status', class: 'col-span-3' },
+  { label: 'Mushrooms', key: 'numberOfMushrooms', class: 'col-span-1' }
 ]
 
 const page = ref(0)
