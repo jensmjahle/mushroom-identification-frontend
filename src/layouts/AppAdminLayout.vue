d2780d71aa11017a030aadc855fb966f1b7fb008
jensmjahle <126790845+jensmjahle@users.noreply.github.com>
2025-05-02 20:04:48 +0200

    feat: integrate global socket management and add buffer/process polyfills

--- src/layouts/AppAdminLayout.vue
+++ src/layouts/AppAdminLayout.vue
@@ -18,4 +18,41 @@ import SideMenu from "../components/navigation/AdminSideMenu.vue";
 import MobileHamburgerMenu from "@/components/navigation/MobileHamburgerMenu.vue";
 import AdminSideMenuContent from "@/components/navigation/AdminSideMenuContent.vue";
 import NavBar from "@/components/navigation/NavBar.vue";
+
+import {onMounted, onUnmounted} from 'vue';
+import {useRouter} from 'vue-router';
+import {disconnectGlobalSocket, initGlobalSocket} from '@/services/globalSocket';
+import {useToast} from "vue-toastification";
+
+const router = useRouter();
+const toast = useToast();
+const showToast = (message) => {
+  toast.info(message, {
+    timeout: 5000,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+  });
+};
+onMounted(() => {
+  const token = sessionStorage.getItem('jwt');
+  if (!token) return;
+
+  initGlobalSocket(
+      token,
+      (error) => {
+        console.warn('Received WebSocket error for admin:', error);
+        showToast(`${error}`);
+      },
+      (broadcast) => {
+        console.log('Admin broadcast:', broadcast);
+        showToast(`${broadcast.message}`);
+      }
+  );
+});
+
+onUnmounted(() => {
+  disconnectGlobalSocket();
+});
 </script>
+
