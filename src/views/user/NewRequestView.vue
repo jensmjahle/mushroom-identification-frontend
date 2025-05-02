3978fef25d7ff0a317d2a1169bfe084a965b47e8
anderebe <anderebe@stud.ntnu.no>
2025-05-02 10:15:14 +0200

    Add a step 0 to explain to the user the process and other info before starting the request submission

--- src/views/user/NewRequestView.vue
+++ src/views/user/NewRequestView.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="flex flex-col items-center justify-center h-full w-full relative">
-    <div class="absolute top-6 w-full flex justify-center">
+    <div
+      v-if="currentStep !== 0"
+      class="absolute top-6 w-full flex justify-center"
+    >
       <StepIndicator :step="currentStep" />
     </div>
+
     <div class="flex items-center justify-center w-full h-full">
-      <StepOne v-if="currentStep === 1" @next="goToStepTwo" />
+      <StepZero v-if="currentStep === 0" @next="handleNextStep" />
+      <StepOne v-if="currentStep === 1" @next="goToStepTwo" /> 
       <StepTwo v-else-if="currentStep === 2" :referenceCode="userCode" @next="currentStep++" @back="currentStep--" />
       <StepThree v-else-if="currentStep === 3" :referenceCode="userCode" @back="currentStep--" />
     </div>
@@ -14,15 +19,20 @@
 <script setup>
 import { ref } from 'vue'
 import StepIndicator from '@/components/User/steps/StepIndicator.vue'
+import StepZero from '@/components/User/steps/StepZero.vue'
 import StepOne from '@/components/User/steps/StepOne.vue'
 import StepTwo from '@/components/User/steps/StepTwo.vue'
 import StepThree from '@/components/User/steps/StepThree.vue'
 
-const currentStep = ref(1)
+const currentStep = ref(0)
 const userCode = ref(null)
 
 function goToStepTwo(result) {
   userCode.value = result
   currentStep.value = 2
+}
+
+function handleNextStep() {
+  currentStep.value++
 }
 </script>
