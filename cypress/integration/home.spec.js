3bfb744f80c4166c0be21191f26c9e0e428185b5
anderebe <anderebe@stud.ntnu.no>
2025-05-09 19:05:01 +0200

    Delete unecessary demo file and folder

--- cypress/integration/home.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Home Page', () => {
-  it('should load the home page correctly', () => {
-    cy.visit('/')  // Visit the home page
-    cy.contains('Welcome')  // Check if the page contains a welcome message
-  })
-})
