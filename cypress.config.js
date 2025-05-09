1a4dd6752041b4313e367e7fdc821a77d39da14f
anderebe <anderebe@stud.ntnu.no>
2025-05-09 19:32:04 +0200

    Add correct config setup for cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',  
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // You can add custom event listeners here if needed (optional)
+      on('test:before:run', (details) => {
+        console.log(`Starting test: ${details.title}`);
+      });
+      return config;
     },
+    supportFile: 'cypress/support/e2e.js', 
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    video: false,
   },
 });
--- vite.config.js
+++ vite.config.js
@@ -31,4 +31,7 @@ export default defineConfig({
       reportsDirectory: './coverage'
     }
   },
+  build: {
+    target: 'esnext',
+  }
 });
