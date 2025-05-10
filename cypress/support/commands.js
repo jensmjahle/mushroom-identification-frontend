c422642c24598965c339f1612ceb219217a966ad
anderebe <anderebe@stud.ntnu.no>
2025-05-10 14:22:25 +0200

    Add proccess images to commands in cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,6 @@
 import enJson from '../../src/locales/en.json';
+import 'cypress-file-upload';
+
 
 Cypress.Commands.add('mockI18n', () => {
     cy.window().then((win) => {
@@ -16,3 +18,27 @@ Cypress.Commands.add('openSettingsAndChangeLanguage', () => {
     cy.get('#settings-button').click();
     cy.wait(500);
 });
+
+// Mock ExifReader and processImageFiles in Cypress
+Cypress.Commands.add('mockImageProcessing', () => {
+  cy.window().then((window) => {
+    // Mock ExifReader
+    window.ExifReader = {
+      load: cy.stub().returns({
+        "0th": {
+          271: { description: 'Make', value: 'Canon' },
+          272: { description: 'Model', value: 'Canon EOS' },
+        }
+      })
+    };
+
+    // Mock processImageFiles to return mocked files
+    window.processImageFiles = cy.stub().resolves({
+      processedFiles: [
+        new File(['dummy content'], 'mushroom1.png', { type: 'image/jpeg' })
+      ],
+      error: null
+    });
+  });
+});
+
