09e8a491da24b06e71fc4533da0d1bfa4f719710
anderebe <anderebe@stud.ntnu.no>
2025-05-10 11:53:01 +0200

    Move commands to the commands.js cypress/support

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,18 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import enJson from '../../src/locales/en.json';
+
+Cypress.Commands.add('mockI18n', () => {
+    cy.window().then((win) => {
+        if (win.$app) {
+            win.$app.config.globalProperties.$i18n = {
+                t: (key) => enJson[key] || key,
+            };
+        }
+    });
+});
+
+Cypress.Commands.add('openSettingsAndSelectEnglish', () => {
+    cy.get('#settings-button').click();
+    cy.get('select').select('en');
+    cy.get('#settings-button').click();
+    cy.wait(500);
+});
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,10 +1,2 @@
-import enJson from '../../src/locales/en.json'; // Adjust the path to where your en.json is located
+import './commands';  // Ensure this line is present
 
-Cypress.Commands.add('mockI18n', () => {
-  // Access Vue instance and mock `useI18n` directly
-  cy.window().then((win) => {
-    win.Vue.prototype.$i18n = {
-      t: (key) => enJson[key] || key,  // Use en.json for translations
-    };
-  });
-});
