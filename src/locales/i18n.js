59c1b575abefcf198b760daf5844cd25784d8024
anderebe <anderebe@stud.ntnu.no>
2025-05-25 19:16:39 +0200

    Make the i18n automaticly pick up on new .json language files and add them to the available languages

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -1,26 +1,33 @@
 import { createI18n } from 'vue-i18n'
-import en from './en.json'
-import no from './no.json'
+
+// Dynamically load all JSON files in the current folder
+const languageFiles = import.meta.glob('./*.json', { eager: true })
+
+const messages = {}
+
+for (const path in languageFiles) {
+  const matched = path.match(/\.\/([a-z]{2})\.json$/)
+  if (matched) {
+    const locale = matched[1]
+    messages[locale] = languageFiles[path].default
+  }
+}
 
 const i18n = createI18n({
   legacy: false,
-  locale: 'en', 
+  locale: 'en',
   fallbackLocale: 'en',
-  messages: {
-    en,
-    no
-  }
+  messages,
 })
 
 export function updateI18nLocale() {
   const stored = sessionStorage.getItem('locale')
-  if (stored && ['en', 'no'].includes(stored)) {
+  if (stored && Object.keys(messages).includes(stored)) {
     i18n.global.locale.value = stored
   } else {
     i18n.global.locale.value = 'en'
     sessionStorage.setItem('locale', 'en')
   }
 }
-
 
 export default i18n
